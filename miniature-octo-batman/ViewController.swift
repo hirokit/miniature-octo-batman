829e9fe8b121903b2346d55f1b282b6b4756a808
hirokit <hirokit@alumni.stanford.edu>
2015-05-12 11:21:59 +0900

    Generating QR code (still in test-version)
    First edit.

--- miniature-octo-batman/ViewController.swift
+++ miniature-octo-batman/ViewController.swift
@@ -1,8 +1,8 @@
 //
 //  ViewController.swift
-//  miniature-octo-batman
+//  QRCodeGen
 //
-//  Created by TaketomoHiroki on 5/12/15.
+//  Created by TaketomoHiroki on 5/11/15.
 //  Copyright (c) 2015 TaketomoHiroki. All rights reserved.
 //
 
@@ -10,6 +10,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var qrcodeImage: CIImage!
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    @IBOutlet weak var imgQRCode: UIImageView!
+    
+    
+    @IBOutlet weak var btnAction: UIButton!
+    
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    
+    @IBAction func performButtonAction(sender: AnyObject) {
+        if qrcodeImage == nil {
+            if textField.text == " " {
+                return
+            }
+            let data = textField.text.dataUsingEncoding(NSISOLatin1StringEncoding, allowLossyConversion: false)
+            
+            let filter = CIFilter(name: "CIQRCodeGenerator")
+            
+            filter.setValue(data, forKey: "inputMessage")
+            
+            filter.setValue("Q", forKey: "inputCorrectionLevel")
+            
+            qrcodeImage = filter.outputImage
+            
+            imgQRCode.image = UIImage(CIImage: qrcodeImage)
+            
+            textField.resignFirstResponder()
+            
+            btnAction.setTitle("Clear", forState: UIControlState.Normal)
+            
+            slider.hidden = false
+            
+            displayQRCodeImage()
+            
+        } else {
+            imgQRCode.image = nil
+            qrcodeImage = nil
+            btnAction.setTitle("Generate", forState: UIControlState.Normal)
+        }
+    }
+    
+    func displayQRCodeImage() {
+        let scaleX = imgQRCode.frame.size.width / qrcodeImage.extent().size.width
+        let scaleY = imgQRCode.frame.size.height / qrcodeImage.extent().size.height
+        
+        let transformedImage = qrcodeImage.imageByApplyingTransform(CGAffineTransformMakeScale(scaleX, scaleY))
+        
+        imgQRCode.image = UIImage(CIImage: transformedImage)
+        
+        
+    }
+    
+    @IBAction func changeImageViewScale(sender: AnyObject) {
+        imgQRCode.transform = CGAffineTransformMakeScale(CGFloat(slider.value), CGFloat(slider.value))
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
